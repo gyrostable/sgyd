5eed92241f9eed59b0933f7758d72d5569f49320
Daniel Perez <daniel@perez.sh>
2024-07-25 14:50:04 +0100

    Deploy new sgyd distributor

--- script/deployments/DeployGydDistributor.s.sol
+++ script/deployments/DeployGydDistributor.s.sol
@@ -14,6 +14,6 @@ contract DeployGydDistributor is Deployment {
             type(GydDistributor).creationCode,
             abi.encode(token, deployer, deployer, 0.5e18, 1 days, escrow)
         );
-        _deploy("DummyGydDistributor", creationCode);
+        _deploy("DummyGydDistributorV2", creationCode);
     }
 }
